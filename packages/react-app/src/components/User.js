3b154cf183c87992f74a109aa3848831086a5063
Makoto Inoue <2630+makoto@users.noreply.github.com>
2021-02-25 07:19:04 +0000

    Resolve name

--- packages/react-app/src/components/User.js
+++ packages/react-app/src/components/User.js
@@ -4,17 +4,29 @@ import { Body, Button, Header, Image, IconImage, Link, InternalLink } from ".";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import { chain } from "lodash";
+import { getProvider } from "../utils"
+
 export const SwapLinkContainer = styled.span`
   margin-right: 1em;
 `;
 
 function User() {
-  let { account } = useParams();
-  
+  let { account } = useParams()
+  const [ userAddress, setUserAddress ] = useState(false);
+
+  if(account.match(/^0x/) && account.length === 42){
+    setUserAddress(account)
+  }else{
+    const mainnetProvider = getProvider()
+    mainnetProvider.resolveName(account).then((a)=>{
+      setUserAddress(a)
+    })
+  }
   return (
     <Body>
       <h3>{account}</h3>
       <ul>
+        <li> { userAddress }</li>
       </ul>
     </Body>
   )
